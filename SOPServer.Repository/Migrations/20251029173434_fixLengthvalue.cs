53a299ddb134d5995e9a105fc86e9dda8e960bd2
lion3993vn <lion3993vn@gmail.com>
2025-10-30 00:35:02 +0700

    fix: length value

---
+++ SOPServer.Repository/Migrations/20251029173434_fixLengthvalue.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOPServer.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixLengthvalue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "AISettings",
+                type: "nvarchar(max)",
+                maxLength: 2147483647,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "AISettings",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 2147483647);
+        }
+    }
+}

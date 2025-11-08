ed2fc3de16ac58b84e7adb9e0a36fbd846e24302
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-11-08 22:02:58 +0700

    add: get top contributor

--- SOPServer.Service/Services/Interfaces/IPostService.cs
+++ SOPServer.Service/Services/Interfaces/IPostService.cs
@@ -13,5 +13,6 @@ namespace SOPServer.Service.Services.Interfaces
         Task<BaseResponseModel> GetPostByIdAsync(long id);
         Task<BaseResponseModel> GetPostByUserIdAsync(PaginationParameter paginationParameter, long userId);
         Task<BaseResponseModel> GetPostsByHashtagIdAsync(PaginationParameter paginationParameter, long hashtagId);
+        Task<BaseResponseModel> GetTopContributorsAsync(PaginationParameter paginationParameter);
     }
 }

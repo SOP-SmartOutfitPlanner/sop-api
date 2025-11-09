3b03e11da3b4b3bd19d43d88f01e6925e27c0641
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-11-09 21:54:53 +0700

    add: collection, collection outfit

---
+++ SOPServer.Repository/Migrations/20251109145414_Collection.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOPServer.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Collection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collection",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<long>(type: "bigint", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Collection__3214EC07", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Collection_User",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CollectionOutfit",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CollectionId = table.Column<long>(type: "bigint", nullable: false),
+                    OutfitId = table.Column<long>(type: "bigint", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__CollectionOutfit__3214EC07", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CollectionOutfit_Collection",
+                        column: x => x.CollectionId,
+                        principalTable: "Collection",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CollectionOutfit_Outfit",
+                        column: x => x.OutfitId,
+                        principalTable: "Outfit",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collection_UserId",
+                table: "Collection",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionOutfit_CollectionId",
+                table: "CollectionOutfit",
+                column: "CollectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionOutfit_OutfitId",
+                table: "CollectionOutfit",
+                column: "OutfitId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollectionOutfit");
+
+            migrationBuilder.DropTable(
+                name: "Collection");
+        }
+    }
+}

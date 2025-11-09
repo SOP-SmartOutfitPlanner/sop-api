30cbe830e066ed6c1c45e6147e7806db4eb95d39
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-11-09 22:30:22 +0700

    update: FK for Outfit

---
+++ SOPServer.Repository/Migrations/20251109153001_UpdateOutfit.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOPServer.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOutfit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "OutfitId1",
+                table: "CollectionOutfit",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionOutfit_OutfitId1",
+                table: "CollectionOutfit",
+                column: "OutfitId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CollectionOutfit_Outfit_OutfitId1",
+                table: "CollectionOutfit",
+                column: "OutfitId1",
+                principalTable: "Outfit",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CollectionOutfit_Outfit_OutfitId1",
+                table: "CollectionOutfit");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CollectionOutfit_OutfitId1",
+                table: "CollectionOutfit");
+
+            migrationBuilder.DropColumn(
+                name: "OutfitId1",
+                table: "CollectionOutfit");
+        }
+    }
+}

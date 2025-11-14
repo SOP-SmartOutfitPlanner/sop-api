ec5be9a4d4b36acbf0125c04526e74559bd6a19d
lion3993vn <lion3993vn@gmail.com>
2025-11-14 12:54:16 +0700

    fix: cicd with firebase admin

---
+++ SOPServer.API/Configurations/FirebaseAppConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.EntityFrameworkCore;
+using SOPServer.Repository.DBContext;
+
+namespace SOPServer.API.Configurations
+{
+    public static class FirebaseAppConfiguration
+    {
+        public static void AddFirebaseAppConfiguration(
+            IWebHostEnvironment environment)
+        {
+            var pathFirebase = string.Empty;
+
+            if(!environment.IsDevelopment())
+            {
+                pathFirebase = Environment.GetEnvironmentVariable("PATH_FIREBASE_ADMIN");
+            }
+            else
+            {
+                pathFirebase = "firebase-adminsdk.json";
+            }
+
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(pathFirebase)
+            });
+        }
+    }
+}

205fa10dee6c19ff353d3accbed14a4b7c1ce133
Tran Quang Huy (K17 HCM) <91615324+huytqse170597@users.noreply.github.com>
2025-11-15 21:29:35 +0700

    add: get all report with filter

--- SOPServer.Service/BusinessModels/ReportCommunityModels/ReportFilterModel.cs
+++ SOPServer.Service/BusinessModels/ReportCommunityModels/ReportFilterModel.cs
@@ -5,6 +5,7 @@ namespace SOPServer.Service.BusinessModels.ReportCommunityModels
     public class ReportFilterModel
     {
         public ReportType? Type { get; set; }
+        public ReportStatus? Status { get; set; }
         public DateTime? FromDate { get; set; }
         public DateTime? ToDate { get; set; }
     }
--- SOPServer.Service/Services/Interfaces/IReportCommunityService.cs
+++ SOPServer.Service/Services/Interfaces/IReportCommunityService.cs
@@ -9,6 +9,7 @@ namespace SOPServer.Service.Services.Interfaces
     {
         Task<BaseResponseModel> CreateReportAsync(ReportCommunityCreateModel model);
         Task<BaseResponseModel> GetPendingReportsAsync(ReportFilterModel filter, PaginationParameter pagination);
+        Task<BaseResponseModel> GetAllReportsAsync(ReportFilterModel filter, PaginationParameter pagination);
         Task<BaseResponseModel> GetReportDetailsAsync(long reportId);
         Task<BaseResponseModel> ResolveNoViolationAsync(long reportId, long adminId, ResolveNoViolationModel model);
         Task<BaseResponseModel> ResolveWithActionAsync(long reportId, long adminId, ResolveWithActionModel model);

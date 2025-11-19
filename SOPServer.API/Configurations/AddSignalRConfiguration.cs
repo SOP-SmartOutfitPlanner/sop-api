e056862b223b8093709a5b6a3fd0724068ecc8aa
lion3993vn <lion3993vn@gmail.com>
2025-11-19 23:23:28 +0700

    fix: add hub connection to send data from webhook

---
+++ SOPServer.API/Configurations/AddSignalRConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace SOPServer.API.Configurations
+{
+    public static class SignalRConfiguration
+    {
+        public static IServiceCollection AddSignalRConfiguration(this IServiceCollection services)
+        {
+            services.AddSignalR(options =>
+            {
+                options.EnableDetailedErrors = true;
+                options.MaximumReceiveMessageSize = 1024 * 1024; // 1 MB
+            });
+            return services;
+        }
+    }
+}

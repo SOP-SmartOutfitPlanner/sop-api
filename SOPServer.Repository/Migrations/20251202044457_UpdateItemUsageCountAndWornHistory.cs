7d14154487ac2c66f7ee06d2379d1b063506e050
lion3993vn <lion3993vn@gmail.com>
2025-12-02 12:04:34 +0700

    add: tracking worn item

---
+++ SOPServer.Repository/Migrations/20251202044457_UpdateItemUsageCountAndWornHistory.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOPServer.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateItemUsageCountAndWornHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FrequencyWorn",
+                table: "Item");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsageCount",
+                table: "Item",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WornAtHistoryJson",
+                table: "Item",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsageCount",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "WornAtHistoryJson",
+                table: "Item");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FrequencyWorn",
+                table: "Item",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+    }
+}
